af7fbd9fba82b89796b91ede7a5c5ae01d4cd474
neilkrichi <neilkrichi@gmail.com>
2017-08-16 17:43:38 -0700

    styles and stuff

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 import FontAwesome from 'react-fontawesome'
+import TypeOut from 'react-typeout';
 
 import TopNav from './TopNav'
 import Footer from './Footer'
 import logo from '../NKlogo.png'
 import '../stylesheets/App.css';
 
+const words = ['a creative', 'an enthusiastic', 'an adventurous', 'a passionate'];
+
+
 export default class Home extends Component {
   render() {
     return (
       <div className="App">
         <TopNav />
-        <h1 className='main-header'>Home :)</h1>
+        <div className="intro-text">
+          <h1>Welcome,</h1> <br/>
+          <h3 className="app-intro">
+            My name is Neil and I am <span className='typewriter'><TypeOut words={words} typeSpeed={150} /></span> designer and developer.
+          </h3> <br/>
+        <button className="cta-button"><Link to='/projects'>Let's see what you got, Neil</Link></button>
+        </div>
         <Footer />
       </div>
-    );
-  }
+
+  );
+}
 }
