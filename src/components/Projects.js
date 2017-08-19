76c32d20be5e84451c30fe4fdb9e22d1224bed2a
neilkrichi <neilkrichi@gmail.com>
2017-08-18 19:15:36 -0700

    projects page in progress

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,7 +11,28 @@ export default class Projects extends Component {
       <div>
         <TopNav />
         <div className='projects-page'>
-          <h1>Here are my projects</h1>
+          <div className="projects-row">
+            <div className="col first-item">
+              Box 1
+            </div>
+            <div className="col second-item">
+              Box 2
+            </div>
+            <div className="col third-item">
+              Box 3
+            </div>
+          </div>
+          <div className="projects-row">
+            <div className="col fourth-item">
+              Box 1
+            </div>
+            <div className="col fifth-item">
+              Box 2
+            </div>
+            <div className="col sixth-item">
+              Box 3
+            </div>
+          </div>
         </div>
         <Footer />
       </div>
