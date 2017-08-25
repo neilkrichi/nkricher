b27e49002bb6e2f7017d0bd0d1449577499ac527
neilkrichi <neilkrichi@gmail.com>
2017-08-24 19:19:47 -0700

    projects page styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -17,7 +17,7 @@ export default class Projects extends Component {
           </div>
           <div className="projects-row">
             <div className="col first-item">
-              Box 1
+              <h2 className="surflife">Surflife</h2>
             </div>
             <div className="col second-item">
               Box 2
@@ -26,13 +26,14 @@ export default class Projects extends Component {
               Box 3
             </div>
             <div className="col fourth-item">
-              Box 1
+              Box 4
             </div>
+
             <div className="col fifth-item">
-              Box 2
+              Box 5
             </div>
             <div className="col sixth-item">
-              Box 3
+              Box 6
             </div>
           </div>
         </div>
