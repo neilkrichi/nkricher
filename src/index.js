ea255be0423359e512d5f7cf212ebc63d1e699d5
neilkrichi <neilkrichi@gmail.com>
2017-09-01 17:38:49 -0700

    added GA

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import './index.css'
 import Home from './components/Home'
 import Projects from './components/Projects'
@@ -9,17 +10,26 @@ import NoMatch from './components/NoMatch'
 import Watdog from './components/Watdog'
 
 import { Router, Route, browserHistory } from 'react-router'
-
+import ReactGA from 'react-ga'
 import registerServiceWorker from './registerServiceWorker'
+
+
+ReactGA.initialize('UA-105753102-1'); //Unique Google Analytics tracking number
+
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname + window.location.search });
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 ReactDOM.render((
   <Router history={browserHistory}>
-  <Route path="/" component={Home} />
-  <Route path="/projects" component={Projects} />
-  <Route path="/watdog" component={Watdog} />
-  <Route path="/resume" component={Resume} />
-  <Route path="/contact" component={Contact} />
+  <Route path="/" component={Home} onUpdate={logPageView} />
+  <Route path="/projects" component={Projects} onUpdate={logPageView} />
+  <Route path="/watdog" component={Watdog} onUpdate={logPageView} />
+  <Route path="/resume" component={Resume} onUpdate={logPageView} />
+  <Route path="/contact" component={Contact} onUpdate={logPageView} />
   <Route path="*" component={NoMatch}/>
-</Router> ), document.getElementById('root'));
+</Router>
+), document.getElementById('root'));
 
 registerServiceWorker();
