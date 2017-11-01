5ff3897fd0ce70641ec958d8c0dc16c834d02dfa
neilkrichi <neilkrichi@gmail.com>
2017-11-01 15:38:36 -0700

    changed home bg color

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,7 @@ import ReactGA from 'react-ga'
 import TopNav from './TopNav'
 import Footer from './Footer'
 import '../stylesheets/App.css';
+import avatar from '../NKavatar.png'
 
 const words = [' creative', 'n enthusiastic', 'n adventurous', ' passionate'];
 
@@ -23,12 +24,17 @@ export default class Home extends Component {
       <div className="App">
         <TopNav />
         <div className="intro">
-          <h1>Hello, it's me.</h1> <br/>
-          <h3 className="app-intro">
-            My name is Neil and I am a<span className='typewriter'><TypeOut words={words} typeSpeed={100} /></span> designer and developer.
-          </h3> <br/>
-        <Link to='/projects'><button className="cta-button" onClick={() => {this.handleClick()}}>Let's see what you've got, Neil.</button></Link>
+          <div>
+            <img className='avatar-pic' src={avatar} />
+          </div>
+          <div className='intro-text'>
+            <h1>Hello, it's me.</h1> <br/>
+            <h3 className="app-intro">
+              My name is Neil and I am a<span className='typewriter'><TypeOut words={words} typeSpeed={100} /></span> designer and developer.
+            </h3> <br/>
+          </div>
         </div>
+        <Link to='/projects'><button className="cta-button" onClick={() => {this.handleClick()}}>Let's see what you've got, Neil.</button></Link>
       </div>
 
   );
