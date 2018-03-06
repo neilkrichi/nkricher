1a215cf14d12510dbfbd05953ab004995c54e21d
neilkrichi <neilkrichi@gmail.com>
2018-03-06 15:50:26 -0800

    changed address

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,7 +22,7 @@ export default class Contact extends Component {
               <p><strong>Tel:</strong> +1 (514) 834-6382 </p>
               <p><strong>Email:</strong> <a href='mailto:neil.krichi@gmail.com'>neilkrichi@gmail.com</a></p>
               <p><strong>Skype:</strong> neil.krichi</p>
-              <p><strong>Address:</strong> 225 W. 8th Ave #300, Vancouver, BC V5Y 1N3</p>
+              <p><strong>Address:</strong> 186 W. 41st Street, Vancouver, BC V5Y 2S1, Canada</p>
             </div>
           </div>
         </div>
